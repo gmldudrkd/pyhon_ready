5feb9ee8c6c6f54c388135e92ab97d4eb5eeb05c
gmldudrkd <gmldud8100@naver.com>
2021-04-12 02:30:27 +0900

    python test

---
+++ pyhon_test.py
@@ -0,0 +1,11 @@
+# code test
+import time
+start_time = time.time()
+
+# print("Hello\nWorld")
+print('print("Hello\\nWorld")')
+
+
+end_time = time.time()
+print()
+print(">> Run time", end_time-start_time)
