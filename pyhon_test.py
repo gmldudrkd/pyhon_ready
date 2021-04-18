64a3c8b9ecf327ea6fe6b322754f7b6f42338a41
gmldudrkd <gmldud8100@naver.com>
2021-04-18 21:26:33 +0900

    2021 04 18 modify

--- pyhon_test.py
+++ pyhon_test.py
@@ -1,12 +1,15 @@
 # code test
 
-#런타임 구하기
+# 런타임 구하기
 import time
 start_time = time.time()
 
-a, b = input().split()
-print(float(a)**float(b))
+a, b, c = input().split()
+var_arr = [a, b, c]
+print(len(var_arr))
 
+for i in len(var_arr):
+    print(i)
 
 end_time = time.time()
 print()
@@ -42,12 +45,54 @@ print('%o'%n) #8진수
 
 
 
-#문자를 10진수 유니코드(Unicode)로
+# 문자를 10진수 유니코드(Unicode)로
 a = ord(input())
-#유니코드를 +1 해서 코드에 해당하는 문자를 출력
+# 유니코드를 +1 해서 코드에 해당하는 문자를 출력
 print(chr(a+1))
 
-#단어의 연속 출력
+# 단어의 연속 출력
 word, r = input().split()
 print(word*int(r))
+
+# 연산식
+// : 몫구하기
+% : 나머지구하기
+
+# 정확도로 반올림한 값(3째자리에서)
+print(format(a, ".2f"))
+print("%.3f" % round(f1/f2, 3)) > 라운드된 값을 소수점 3째자리까지 노출
+print(format(a/b, ".2f")) > 정확도로 소수점 2자리까지 노출
+
+%.숫자f'%노출할입력값
+ -> %.숫자f : 표시할 소수점 아래자리수를 정함
+ 
+ex)
+ a, b, c = map(int, input().split())
+add = a+b+c
+aver = round(add/3, 3)
+print(add, format("%.2f"%aver))
+
+# 비트연산자
+2진수를 왼쪽부터 넣음
+<< : *2, >> : %2
+print(a << b)
+-> a * 2의b승
+
+# 비교연산자
+xor : 두값이 다를때만 true
+print((a_b and not b_b) or (not a_b and b_b))
+not or : 두 값이 모두 false 일때만 true
+print(not(a_b or b_b))
+
+# 비트단위 논리연산
+~n = -n-1 (tilde, 틸드)
+ex) ~5 = -5-1 = -6
+ -> ~(0101) = 1010 = -6
+비트연산 : int(a) & int(b) => 0101 & 0011 = 0001(3 & 5 = 1)
+
+#3항연산
+a if C else b
+a : C가 참일때 출력
+b : C가 참이 아닐때 출력
+
 '''
